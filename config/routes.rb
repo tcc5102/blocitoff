0c415b06bc2d9655686d44745c7eac38ee8fe2e5
Tyler Cooper <tyler.cooper91@gmail.com>
2017-01-04 17:07:27 -0500

    devise working

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,17 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      # will be home page
+      root 'welcome#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
