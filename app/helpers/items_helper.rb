c3472c44e0afaafbd0503df6246a2b929fa4255d
Tyler Cooper <tyler.cooper91@gmail.com>
2017-01-16 11:41:03 -0500

    distance of time from creation

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,8 @@
 module ItemsHelper
+include ActionView::Helpers::DateHelper
+
+  def days_left(item)
+    "#{distance_of_time_in_words(item.created_at, Time.current)} ago"
+    # "#{distance_of_time_in_words(item.created_at + 7.days, item.created_at)} left"
+  end
 end
