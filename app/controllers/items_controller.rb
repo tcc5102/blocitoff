bd96dc82ed0de928bcf5fbba439d0dbc2afe36a1
Tyler Cooper <tyler.cooper91@gmail.com>
2017-01-15 21:34:48 -0500

    delete items link

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,19 @@ class ItemsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @item = current_user.items.create(item_params)
+    redirect_back fallback_location: {action: "show"}
 
-    if @item.save
-      flash[:notice] = "Item #{@item.name} Now in List"
-      redirect_back fallback_location: {action: "show"}
-    end
+    # if @item.save
+    #   flash[:notice] = "Item #{@item.name} Now in List"
+    #   redirect_back fallback_location: {action: "show"}
+    # end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_back fallback_location: {action: "show"}
   end
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show] do
-    resources :items, only: [:create]
+    resources :items, only: [:create, :destroy]
   end
 
   get 'welcome/index'
